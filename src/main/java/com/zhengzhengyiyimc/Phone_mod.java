d33c291614e8667159de7fb1a100a4ef012b9c0e
zhengzhengyiyi <zhengzhichen44@163.com>
2025-06-22 11:17:51 -0400

    add phone, add langage json

--- src/main/java/com/zhengzhengyiyimc/Phone_mod.java
+++ src/main/java/com/zhengzhengyiyimc/Phone_mod.java
@@ -1,24 +1,24 @@
 package com.zhengzhengyiyimc;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.zhengzhengyiyimc.item.Phone;
+
 public class Phone_mod implements ModInitializer {
 	public static final String MOD_ID = "phone_mod";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final Phone PHONE_ITEM = new Phone(new Item.Settings().maxCount(1).maxCount(3));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, Identifier.of("phone_mod", "phone"), PHONE_ITEM);
 	}
 }
